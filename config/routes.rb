f025794681cf697d7198d50881965da7f8155542
andrew shafer <>
2025-03-13 16:46:20 -0600

    fix routs spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/ask', to: 'request#ask'
-  get '/input', to: 'request#input'
+  get '/ask', to: 'requests#ask'
+  get '/input', to: 'requests#input'
 
 end
